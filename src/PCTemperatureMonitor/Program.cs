cfbd6dc0f32d60ea6a065614c294379a5fb6c8b0
artembal90 <artembal90@gmail.com>
2026-09-07 01:09:35 +0300

    Add application entry point

---
+++ src/PCTemperatureMonitor/Program.cs
@@ -0,0 +1,12 @@
+namespace PCTemperatureMonitor;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+        Application.Run(new MainForm());
+    }
+}
